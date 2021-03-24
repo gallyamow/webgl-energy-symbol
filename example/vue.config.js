4d98216b170447a60849663360ca3fc75ca43141
Ramil Gallyamov <gallyamov.ramil@tatar.ru>
2021-03-24 12:17:24 +0300

    release

--- example/vue.config.js
+++ example/vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   // @see https://github.com/vuejs/vue-cli/issues/2948
   chainWebpack: config => config.resolve.symlinks(false),
   // @see https://cli.vuejs.org/ru/config/#publicpath
-  publicPath: process.env.NODE_ENV === 'production' ? '/webgl-helix-ribbon/example/dist/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/webgl-energy-symbol/example/dist/' : '/'
 }
