c9f910aa2bfb760ca868c0062a6412ec17ff065c
Ramil Gallyamov <gallyamov.ramil@tatar.ru>
2021-03-25 11:34:43 +0300

    release

--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="container" ref="container"></div>
+  <div class="container" ref="container">
+  </div>
 </template>
 
 <script>
 import { EnergySymbolScene, rand, moveVectors, scaleVectors } from 'webgl-energy-symbol'
 import { VECTORS_TREE, VECTORS_FAN, VECTORS_RING, VECTORS_MICRO, GRADIENTS, GRADIENTS_BLURRED } from './symbols'
 
-const TRANSFORMING_ENABLED = true
+const TRANSFORMING_ENABLED = false
+
+/**
+ * @type {FigureOptions}
+ */
+const OPTIONS_FIGURE = {
+  massRange: { min: 5.5, max: 8 },
+  varianceRange: { min: -10, max: 10 },
+}
+
+/**
+ * @type {OutlineOptions}
+ */
+const OPTIONS_OUTLINE = {
+  colors: GRADIENTS,
+  varianceRange: { min: -5, max: 5 },
+  scaleRange: { min: 0.995, max: 1.005 },
+  thicknessRange: { min: 1, max: 1.8 },
+  rotationRange: { min: -Math.PI / 100, max: Math.PI / 100 },
+}
+
+/**
+ * @type {OutlineOptions}
+ */
+const OPTIONS_BLURRED_OUTLINE = {
+  colors: GRADIENTS_BLURRED,
+  varianceRange: { min: -10, max: 10 },
+  scaleRange: { min: 0.995, max: 1.005 },
+  thicknessRange: { min: 9.24, max: 10.36 },
+  rotationRange: { min: -Math.PI / 100, max: Math.PI / 100 },
+}
+
+/**
+ * @type {SpringOptions}
+ */
+const OPTIONS_SPRING = {
+  strengthRange: { min: 0.5, max: 0.7 },
+  dragRange: { min: 0, max: 0 },
+  restRange: { min: 2, max: 4 },
+}
+
+// moving and scaling depends on current size (30 - columns count)
+const multiplier = 1 //window.innerWidth / 30
+const SYMBOLS = [
+  moveVectors(scaleVectors(VECTORS_TREE, multiplier), -500, -400),
+  moveVectors(scaleVectors(VECTORS_RING, multiplier), -500, -400),
+  moveVectors(scaleVectors(VECTORS_MICRO, multiplier), -500, -400),
+  moveVectors(scaleVectors(VECTORS_FAN, multiplier), -500, -400),
+]
 
 export default {
   created () {
@@ -17,54 +66,6 @@ export default {
   },
 
   mounted () {
-    /**
-     * @type {FigureOptions}
-     */
-    const OPTIONS_FIGURE = {
-      massRange: { min: 5.5, max: 8 },
-      varianceRange: { min: -2, max: 2 },
-    }
-
-    /**
-     * @type {OutlineOptions}
-     */
-    const OPTIONS_OUTLINE = {
-      colors: GRADIENTS,
-      varianceRange: { min: -5, max: 5 },
-      scaleRange: { min: 0.995, max: 1.005 },
-      thicknessRange: { min: 1, max: 1.8 },
-      rotationRange: { min: -Math.PI / 100, max: Math.PI / 100 },
-    }
-
-    /**
-     * @type {OutlineOptions}
-     */
-    const OPTIONS_BLURRED_OUTLINE = {
-      colors: GRADIENTS_BLURRED,
-      varianceRange: { min: -10, max: 10 },
-      scaleRange: { min: 0.995, max: 1.005 },
-      thicknessRange: { min: 9.24, max: 10.36 },
-      rotationRange: { min: -Math.PI / 100, max: Math.PI / 100 },
-    }
-
-    /**
-     * @type {SpringOptions}
-     */
-    const OPTIONS_SPRING = {
-      strengthRange: { min: 0.5, max: 0.7 },
-      dragRange: { min: 0, max: 0 },
-      restRange: { min: 2, max: 4 },
-    }
-
-    // moving and scaling depends on current size (30 - columns count)
-    const multiplier = 1 //window.innerWidth / 30
-    const SYMBOLS = [
-      moveVectors(scaleVectors(VECTORS_TREE, multiplier), -500, -400),
-      moveVectors(scaleVectors(VECTORS_RING, multiplier), -500, -400),
-      moveVectors(scaleVectors(VECTORS_MICRO, multiplier), -500, -400),
-      moveVectors(scaleVectors(VECTORS_FAN, 0.5 * multiplier), -9 * multiplier, -4 * multiplier),
-    ]
-
     // noinspection JSCheckFunctionSignatures
     this.energySymbolScene.render(this.$refs.container)
 
@@ -72,7 +73,7 @@ export default {
       SYMBOLS[0],
       OPTIONS_FIGURE,
       OPTIONS_SPRING,
-      10,
+      9,
       OPTIONS_OUTLINE,
       4,
       OPTIONS_BLURRED_OUTLINE
@@ -89,6 +90,7 @@ export default {
     eventTarget.addEventListener('ready', this.onReady)
 
     window.addEventListener('resize', this.onWindowResize)
+    window.addEventListener('wheel', this.onWheel)
   },
 
   destroyed () {
@@ -105,6 +107,11 @@ export default {
     onWindowResize () {
       this.energySymbolScene.resize(window.innerWidth, window.innerHeight)
     },
+
+    onWheel () {
+      const randSymbol = SYMBOLS[Math.floor(rand(0, SYMBOLS.length))]
+      this.energySymbolScene.transformSymbol(randSymbol, OPTIONS_FIGURE.varianceRange)
+    }
   }
 }
 </script>
@@ -113,7 +120,7 @@ export default {
   .container {
     overflow: hidden;
     height: 100%;
-    background: url("../assets/tree-bg.png") no-repeat fixed 85% 99%;
+    /*background: url("../assets/tree-bg.png") no-repeat fixed 85% 99%;*/
     /*background-size: contain;*/
   }
 </style>
